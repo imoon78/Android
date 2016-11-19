bb08508d29cee40ebcb44c4b12a953a01df55a5e
imoon78 <imoon78@gmail.com>
2016-11-19 13:12:51 +0900

    20161119 13:20

--- app/src/main/java/com/imoon/app/imoonapp/member/ListActivity.java
+++ app/src/main/java/com/imoon/app/imoonapp/member/ListActivity.java
@@ -1,9 +1,13 @@
 package com.imoon.app.imoonapp.member;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.imoon.app.imoonapp.R;
 
@@ -21,7 +25,30 @@ public class ListActivity extends AppCompatActivity {
         Log.d("친구 수",String.valueOf(list.size()));
         lv_member = (ListView) findViewById(R.id.lv_member);
         lv_member.setAdapter(new MemberAdapter(this, list));
-        
+
+        // 짧은클릭
+        lv_member.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int i, long l) {
+                Object o = lv_member.getItemAtPosition(i);
+                MemberDTO member = (MemberDTO) o;
+                Toast.makeText(ListActivity.this,"선택한 이름"+member.getName(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ListActivity.this, DetailActivity.class);
+                intent.putExtra("id", member.getId());
+                startActivity(intent);
+            }
+        });
+
+        // 긴클릭
+        lv_member.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View v, int i, long l) {
+                Object o = lv_member.getItemAtPosition(i);
+                MemberDTO member = (MemberDTO) o;
+                Toast.makeText(ListActivity.this,"삭제할 이름"+member.getName(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
     }
 }
