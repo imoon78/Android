875fd42c7c53f86d5785ee1dd097068f50d196fc
imoon78 <imoon78@gmail.com>
2016-11-19 12:53:09 +0900

    점심먹자

--- app/src/main/java/com/imoon/app/imoonapp/member/ListActivity.java
+++ app/src/main/java/com/imoon/app/imoonapp/member/ListActivity.java
@@ -3,14 +3,15 @@ package com.imoon.app.imoonapp.member;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
 import com.imoon.app.imoonapp.R;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
-
     MemberService service;
+    ListView lv_member;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +19,9 @@ public class ListActivity extends AppCompatActivity {
         service  = new MemberServiceImpl(this.getApplicationContext());
         ArrayList<MemberDTO> list = service.list();
         Log.d("친구 수",String.valueOf(list.size()));
+        lv_member = (ListView) findViewById(R.id.lv_member);
+        lv_member.setAdapter(new MemberAdapter(this, list));
+        
+
     }
 }
