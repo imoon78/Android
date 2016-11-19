b4ba93cb2c1db1ce138b0659e572dc7d5d6afb0e
imoon78 <imoon78@gmail.com>
2016-11-19 14:55:24 +0900

    UI스킨

--- app/src/main/java/com/imoon/app/imoonapp/MainActivity.java
+++ app/src/main/java/com/imoon/app/imoonapp/MainActivity.java
@@ -2,48 +2,28 @@ package com.imoon.app.imoonapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.Window;
 
-import com.imoon.app.imoonapp.clac.CalcActivity;
-import com.imoon.app.imoonapp.member.JoinActivity;
-import com.imoon.app.imoonapp.member.LoginActivity;
+import com.imoon.app.imoonapp.intro.IntroActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button bt_calc,bt_join,bt_login;  // ;콜론은 cpu 한번 다니옴
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        bt_calc = (Button) findViewById(R.id.bt_clac);
-        bt_join = (Button) findViewById(R.id.bt_join);
-        bt_login = (Button) findViewById(R.id.bt_login);
-
-        // 클릭 리스너 등록
-        bt_calc.setOnClickListener(this);
-        bt_join.setOnClickListener(this);
-        bt_login.setOnClickListener(this);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this,IntroActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bt_clac:
-                Intent intent = new Intent(this.getApplicationContext(), CalcActivity.class);
-                this.startActivity(intent);
-                break;
-            case R.id.bt_join:
-                Intent intent2 = new Intent(this.getApplicationContext(), JoinActivity.class);
-                this.startActivity(intent2);
-                break;
-            case R.id.bt_login:
-                Intent intent3 = new Intent(this.getApplicationContext(), LoginActivity.class);
-                this.startActivity(intent3);
-                break;
-        }
-   }
+
 }
---
+++ app/src/main/java/com/imoon/app/imoonapp/intro/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.imoon.app.imoonapp.intro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.imoon.app.imoonapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
