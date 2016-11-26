e8c89187f960f8b7c51914cf7a7af2255d1b4d81
imoon78 <imoon78@gmail.com>
2016-11-26 18:03:36 +0900

    html로디자인

--- app/src/main/java/com/imoon/app/imoonapp/util/WebActivity.java
+++ app/src/main/java/com/imoon/app/imoonapp/util/WebActivity.java
@@ -2,6 +2,7 @@ package com.imoon.app.imoonapp.util;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,13 +17,13 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         wv_test = (WebView) findViewById(R.id.wv_test);
         wv_test.setWebViewClient(new WebViewClient()); // 이걸 안해주면 새창이 뜸
+        wv_test.setWebChromeClient(new WebChromeClient());
+        //wv_test.loadUrl("http://www.naver.com");
+        wv_test.loadUrl("file:///android_asset/html/index.html");
 
-        wv_test.loadUrl("http://www.naver.com");
+
+
         WebSettings webSettings = wv_test.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
-
-
-
     }
 }
